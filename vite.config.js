573cb03fa52b43f9dbe93dc4970fd71a63be7548
Ballu <webindiastack@gmail.com>
2026-09-18 11:04:48 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
   base: '/apex_motors/',
-
-  server: {
-    port: 3000,
-    open: true
-  }
-});
+})
